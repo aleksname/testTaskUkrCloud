45badf49f85bfa8ae1a960c7d6d88f1b34335099
aleksname <aleksname.v2@gmail.com>
2025-02-11 11:57:25 +0200

    feat: return to the last commit

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -1,15 +1,43 @@
 <template>
-    <MainContainer>
-        <h1>About us</h1>
+    <MainContainer >
+        <div className="testContainer">
+            <h1>test filter</h1>
+            <div>
+                <ul>
+                    <li v-for="product in products"
+                        :key="product"
+                    >
+                    {{ product.name }}
+                    </li>
+                </ul>
+            </div>
+        </div>
     </MainContainer>
 </template>
 
 <script>
 import MainContainer from '@/components/MainContainer.vue';
+import { ref } from 'vue';
 
 export default {
     components: {
         MainContainer,
+    },
+
+    setup() {
+        const products = ref([
+            {id: 1, name: 'Apple'},
+            {id: 2, name: 'Banana'},
+            {id: 3, name: 'Kivi'},
+        ])
+
+        return {products}
     }
 }
 </script>
+
+<style>
+    .testContainer{
+        color:white;
+    }
+</style>
--- src/views/FilmDetailView.vue
+++ src/views/FilmDetailView.vue
@@ -1,38 +1,29 @@
 <template>
     <MainContainer>
+        <!-- <img :src="'https://image.tmdb.org/t/p/w500' + movie.backdrop_path" alt="mediaCardBG" className="mediaCardBG"> -->
         <div className="wrapperComponent _detailViev">
-            <FilmDetailBackRouting />
-            <div v-if="movie" className="detailMediaCardContainer">
+            <FilmDetailBackRouting/>
+            <div v-if="movie" className="detailMediaCardContainer"   >
                 <div class="detailMediaCard">
-                    <img :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path" alt="Movie poster" className="mediaCardDetail" />
+                    <img :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path" alt="Movie poster" className="mediaCardDetail"   />
                     <div class="detailMediaOvervievContainer">
                         <h1 className="detailMediaCardTitle">{{ movie.title }}</h1>
-                        <h1 className="detailMediaCardSubTitle">{{ 'Популярність ' + movie.popularity }}</h1>
-                        <h1 className="detailMediaCardSubTitle">{{ 'Дата виходу  ' + movie.release_date }}</h1>
-                        <h1 className="detailMediaCardSubTitle">{{ 'Оцінка глядачів  ' + movie.vote_average }}</h1>
-                        <h1 className="detailMediaCardSubTitle">{{ 'Оцінок  ' + movie.vote_count }}</h1>
+                        <h1 className="detailMediaCardSubTitle">{{ 'Популярність ' + `${movie.popularity} `}}</h1>
+                        <h1 className="detailMediaCardSubTitle">{{ 'Дата виходу  ' + `${movie.release_date} `}}</h1>
+                        <h1 className="detailMediaCardSubTitle">{{ 'Оцінка глядачів  ' + `${movie.vote_average} `}}</h1>
+                        <h1 className="detailMediaCardSubTitle">{{ 'Оцінок  ' + `${movie.vote_count} `}}</h1>
                     </div>
                 </div>
-
-                <!-- Кнопка для відкриття відео -->
                 <div class="startOvervievContainer">
-                    <button type="button" className="startOverviev" @click="openVideo">Почати перегляд</button>
+                    <button type="button" className="startOverviev" @click="tempAlert()" >Почати перегляд</button>
                 </div>
-
+                <!-- <video controls width="100%">
+                    <source src="../../public/12672853_2160_3840_30fps.mp4"/>
+                </video> -->
                 <p className="detailMediaCardOverview">{{ movie.overview }}</p>
             </div>
             <div v-else>
-                <p>Завантаження...</p>
-            </div>
-        </div>
-
-        <!-- Модальне вікно з відео -->
-        <div v-if="isVideoOpen" class="modal-overlay" @click="closeVideo">
-            <div class="modal-content" @click.stop>
-                <button class="close-btn" @click="closeVideo">&times;</button>
-                <video controls autoplay width="100%">
-                    <source src="../../public/12672853_2160_3840_30fps.mp4" />
-                </video>
+              <p>Завантаження...</p>
             </div>
         </div>
     </MainContainer>
@@ -41,46 +32,42 @@
 <script>
 import MainContainer from '@/components/MainContainer.vue';
 import FilmDetailBackRouting from '@/components/FilmDetail-BackRouting.vue';
-
 export default {
     components: {
         MainContainer,
         FilmDetailBackRouting,
     },
+
     data() {
         return {
-            movie: null,
-            isVideoOpen: false, // Стан модального вікна
+        movie: null,
         };
     },
     mounted() {
         const movieId = this.$route.params.id;
         const options = {
-            method: 'GET',
-            headers: {
-                accept: 'application/json',
-                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNDUzMThlMjY3NWE1YmRhMWE3OTEyM2E0MWQ2ZGM0YiIsIm5iZiI6MTczOTA4Njk1MS45ODIsInN1YiI6IjY3YTg1YzY3YjkzNjBjM2UzM2UwODYxNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lcHcH8zZoo2w7QBLGSSrD57eDcHrdtxhWyQZJ-_ZTSM',
-            },
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNDUzMThlMjY3NWE1YmRhMWE3OTEyM2E0MWQ2ZGM0YiIsIm5iZiI6MTczOTA4Njk1MS45ODIsInN1YiI6IjY3YTg1YzY3YjkzNjBjM2UzM2UwODYxNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lcHcH8zZoo2w7QBLGSSrD57eDcHrdtxhWyQZJ-_ZTSM',
+        },
         };
 
         fetch(`https://api.themoviedb.org/3/movie/${movieId}?language=en-US`, options)
-            .then((res) => res.json())
-            .then((data) => {
-                this.movie = data;
-            })
-            .catch((err) => console.error(err));
+        .then((res) => res.json())
+        .then((data) => {
+            this.movie = data;
+        })
+        .catch((err) => console.error(err));
     },
-    methods: {
-        openVideo() {
-            this.isVideoOpen = true;
-        },
-        closeVideo() {
-            this.isVideoOpen = false;
-        },
-    },
-};
-</script>
 
+    methods: {
+        tempAlert() {
+            alert('^-^')
+        }
+    }
+    };
+</script>
 
 <style>
 ._detailViev{
@@ -158,43 +145,4 @@ export default {
     font-weight: 400;
     line-height: 30px;
 }
-
-
-/* Затемнений фон */
-.modal-overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-    background: rgba(0, 0, 0, 0.8);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 1000;
-}
-
-/*  модальне відео вікно */
-.modal-content {
-    position: relative;
-    background: #000;
-    border-radius: 10px;
-    max-width: 80%;
-}
-
-.close-btn {
-    position: absolute;
-    top: 10px;
-    right: 15px;
-    background: none;
-    border: none;
-    color: white;
-    font-size: 30px;
-    cursor: pointer;
-}
-
-.close-btn:hover {
-    color: red;
-}
-
 </style>
