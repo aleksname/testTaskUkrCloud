f8906369a1662bd9f5cd247d20478f58446d38cf
aleksname <aleksname.v2@gmail.com>
2025-02-11 13:37:12 +0200

    feat: adding test filter in page About, next step: fix Up/Low case & addint to /home page

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -1,13 +1,17 @@
 <template>
-    <MainContainer >
-        <div className="testContainer">
+    <MainContainer>
+        <div class="testContainer">
             <h1>test filter</h1>
             <div>
+                <input 
+                    type="search"
+                    placeholder="Пошук елментів"
+                    v-model="query"
+                >
+                <div class="">{{  query}}</div>
                 <ul>
-                    <li v-for="product in products"
-                        :key="product"
-                    >
-                    {{ product.name }}
+                    <li v-for="product in queryElement" :key="product">
+                        {{ product }}
                     </li>
                 </ul>
             </div>
@@ -17,27 +21,31 @@
 
 <script>
 import MainContainer from '@/components/MainContainer.vue';
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 export default {
     components: {
         MainContainer,
     },
-
     setup() {
+        const query = ref('')
+        const queryElement = computed(() => {
+            return products.value.filter((product) => product.indexOf(query.value) !== -1)
+        })
         const products = ref([
-            {id: 1, name: 'Apple'},
-            {id: 2, name: 'Banana'},
-            {id: 3, name: 'Kivi'},
-        ])
+            'Apple',
+            'Banana',
+            'ets..',
+            'ets2..',
+        ]);
+        return { products,query,queryElement };
 
-        return {products}
     }
-}
+};
 </script>
 
 <style>
-    .testContainer{
-        color:white;
-    }
+.testContainer {
+    color: white;
+}
 </style>
